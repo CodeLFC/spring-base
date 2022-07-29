dd9131e405a726f306520aebcb24d667f8f3aa8e
LFC_dorm <1240902193@qq.com>
2022-07-29 19:04:22 +0800

    删除QPS控制，将限流交由网关

--- src/main/java/gaozhi/online/base/interceptor/InterceptorConfig.java
+++ src/main/java/gaozhi/online/base/interceptor/InterceptorConfig.java
@@ -14,16 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     private PropertyInterceptor authInterceptor;
-    private QPSInterceptor qpsInterceptor;
 
     @Autowired
     public void setAuthInterceptor(PropertyInterceptor authInterceptor) {
         this.authInterceptor = authInterceptor;
-    }
-
-    @Autowired
-    public void setQpsInterceptor(QPSInterceptor qpsInterceptor) {
-        this.qpsInterceptor = qpsInterceptor;
     }
 
     /**
@@ -34,6 +28,5 @@ public class InterceptorConfig implements WebMvcConfigurer {
      */
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
-        registry.addInterceptor(qpsInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
     }
 }
